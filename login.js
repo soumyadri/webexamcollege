581cc8d0a0a55645746fa0cc28936c3d53a80bbd
Soumyadri Das <dadsoumyadri@gmail.com>
2021-06-22 23:51:28 +0530

    Logout functionality added

--- login.js
+++ login.js
@@ -4,7 +4,10 @@ function funcLogin(){
 
     var loginData = localStorage.getItem("Registration");
     var loginLocalstorage = JSON.parse(loginData);
-    console.log(loginLocalstorage)
+    console.log(loginLocalstorage);
+
+    localStorage.setItem("auth", JSON.stringify({auth: true}));
+
     if(loginEmail == loginLocalstorage.emailRegister && loginPassword == loginLocalstorage.passwordRegister){
         alert("Login Successful");
         window.location.href = "dashboard.html";
